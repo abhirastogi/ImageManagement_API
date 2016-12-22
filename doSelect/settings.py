702269f2925ec6e458cca3d5db9f60ec29e6910a
Abhishek <abhishek.rastogi1910@gmail.com>
2016-12-22 18:41:56 +0530

    change database engine

--- doSelect/settings.py
+++ doSelect/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'doSelect.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+      'default': {
+            'ENGINE': 'django.db.backends.postgresql'       , 
+            'NAME': 'db1',
+            'USER': 'abhishek',
+            'PASSWORD': 'abhishek',
+            'HOST': 'localhost',
+            'PORT': '', 
+        }
 }
 
 
