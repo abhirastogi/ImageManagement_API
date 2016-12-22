c108f25daf338bd47b7bd53f3da0057926b24fe9
Abhishek <abhishek.rastogi1910@gmail.com>
2016-12-22 17:54:15 +0530

    remove debug

--- image_management/models.py
+++ image_management/models.py
@@ -6,6 +6,4 @@ class Photo(models.Model):
 	file_name = models.CharField(max_length=100)
 	image = models.ImageField(upload_to='photos', max_length=255)
 
-	def save(self, *args, **kwargs):
-		import pdb; pdb.set_trace()
 # Create your models here.
