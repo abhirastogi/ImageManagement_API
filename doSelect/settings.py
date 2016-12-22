624dfb4ec5b9ef8c277c527f65072df0cb9894c5
Abhishek <abhishek.rastogi1910@gmail.com>
2016-12-22 18:57:53 +0530

    change database engine

--- doSelect/settings.py
+++ doSelect/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'doSelect.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES[‘default’] = dj_database_url.config()
 
 
-# DATABASES = {
+
+DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -91,8 +91,8 @@ DATABASES[‘default’] = dj_database_url.config()
 #             'HOST': 'localhost',
 #             'PORT': '', 
 #         }
-# }
-
+}
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
